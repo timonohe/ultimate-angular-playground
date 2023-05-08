3364a2d25924df28057140c67b57ca7a6a8eec73
Timo Nohe <timo.ademeit@gmx.de>
2023-05-08 21:47:24 +0200

    added run script for playground

--- apps/ultimate-angular-playground/src/app/app.module.ts
+++ apps/ultimate-angular-playground/src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {NxWelcomeComponent} from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
