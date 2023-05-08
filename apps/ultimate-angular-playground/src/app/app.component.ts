86882d4341b579e341311a8a00a1cf81ad6d9616
Timo Nohe <timo.ademeit@gmx.de>
2023-05-08 21:42:46 +0200

    initial commit

---
+++ apps/ultimate-angular-playground/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ultimate-angular-playground-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ultimate-angular-playground';
+}
