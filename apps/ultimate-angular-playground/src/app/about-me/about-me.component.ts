9551f445ccaafd9111764bcdacc7a9bf18b38865
Timo Nohe <timo.ademeit@gmx.de>
2023-09-07 12:57:39 +0200

    Finished component styles special selectors

--- apps/ultimate-angular-playground/src/app/about-me/about-me.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ultimate-angular-playground-about-me',
-  templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss'],
-})
-export class AboutMeComponent {}
--- apps/ultimate-angular-playground/src/app/app.module.ts
+++ apps/ultimate-angular-playground/src/app/app.module.ts
@@ -1,19 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { LifecycleExampleComponent } from './lifecycle-example/lifecycle-example.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { RouterModule, Routes } from '@angular/router';
-import { ViewEncapsulationComponent } from './view-encapsulation/view-encapsulation.component';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { AngularFireModule } from '@angular/fire/compat';
-import { environment } from '../environments/environment';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {FooterComponent} from './footer/footer.component';
+import {LifecycleExampleComponent} from './lifecycle-example/lifecycle-example.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {RouterModule, Routes} from '@angular/router';
+import {ViewEncapsulationComponent} from './view-encapsulation/view-encapsulation.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {AngularFireModule} from '@angular/fire/compat';
+import {environment} from '../environments/environment';
 
 const routes: Routes = [
   { path: 'angular-lifecycle', component: LifecycleExampleComponent },
@@ -26,17 +24,14 @@ const routes: Routes = [
     path: 'component-styles',
     loadChildren: () => import('./component-styles/component-styles.module').then(m => m.ComponentStylesModule)
   },
-  { path: 'about', component: AboutMeComponent },
   { path: '', redirectTo: 'angular-lifecycle', pathMatch: 'full' },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     FooterComponent,
     LifecycleExampleComponent,
-    AboutMeComponent,
     ViewEncapsulationComponent,
   ],
   imports: [
